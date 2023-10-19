a1a6758b50f76c44c8f96b2c59760bfb5b9b7105
GinMiraing <ginmiraing@outlook.com>
2023-10-20 00:08:32 +0800

    feat: 引入 Fancybox 依赖

--- app/posts/[id]/page.tsx
+++ app/posts/[id]/page.tsx
@@ -1,9 +1,12 @@
 "use client";
 
+import { Fancybox } from "@fancyapps/ui";
+import "@fancyapps/ui/dist/fancybox/fancybox.css";
 import type { MDXComponents } from "mdx/types";
 import { useMDXComponent } from "next-contentlayer/hooks";
-import Image from "next/image";
+import Image from "next/legacy/image";
 import Link from "next/link";
+import { useEffect } from "react";
 
 import Banner from "@/components/Banner";
 import Navbar from "@/components/Navbar";
@@ -14,11 +17,13 @@ const mdxComponents: MDXComponents = {
   a: ({ href, children }) => <Link href={href as string}>{children}</Link>,
   img: ({ src, alt }) => (
     <Link
+      data-fancybox
       className="relative block h-80 w-full overflow-hidden"
       href={src as string}
       target="_blank"
     >
       <Image
+        priority
         src={`${src}@.webp` as string}
         alt={alt || ""}
         layout="fill"
@@ -38,13 +43,38 @@ export default function Page({ params }: { params: { id: string } }) {
 
   const MDXContent = useMDXComponent(post.body.code);
 
+  useEffect(() => {
+    Fancybox.bind("[data-fancybox]", {
+      groupAll: true,
+      Thumbs: false,
+      Carousel: {
+        transition: "slide",
+      },
+      Images: {
+        zoom: false,
+      },
+      showClass: "f-fadeSlowIn",
+      hideClass: "f-fadeSlowOut",
+      wheel: "slide",
+      Toolbar: {
+        display: {
+          left: [],
+          middle: [],
+          right: ["close"],
+        },
+      },
+    });
+  }, []);
+
   return (
-    <main className="min-h-screen max-w-4xl rounded-sm bg-white pt-16 shadow sm:mx-16 sm:mt-20 sm:min-h-0 sm:pt-0 lg:mx-auto">
-      <Banner />
-      <Navbar />
-      <div className="px-6">
-        <MDXContent components={mdxComponents} />
-      </div>
-    </main>
+    <>
+      <main className="min-h-screen max-w-4xl rounded-sm bg-white pt-16 shadow sm:mx-16 sm:mt-20 sm:min-h-0 sm:pt-0 lg:mx-auto">
+        <Banner />
+        <Navbar />
+        <div className="px-6">
+          <MDXContent components={mdxComponents} />
+        </div>
+      </main>
+    </>
   );
 }
